dcf3047be7907cd96b9ebbb47ee6cf56b885f094
ArranZeyuWang <zywangx@gmail.com>
2020-07-29 14:25:12 +0800

    fix: class defination

--- packages/chart-advisor/src/perception/findScag/grapher.ts
+++ packages/chart-advisor/src/perception/findScag/grapher.ts
@@ -6,8 +6,63 @@ import * as polygon from 'd3-polygon';
 
 // var alphaShape = require('alpha-shape')(alpha, points);
 
-function DisjointSet(this: any) {
-  this.index_ = {};
+export class DisjointSet {
+  index_: any = {};
+  
+  constructor() {
+    this.index_ = {};
+  }
+
+  makeSet(id: string | number) {
+    if (!this.index_[id]) {
+      const created = Node(id);
+      this.index_[id] = created;
+    }
+  };
+
+  find(id: string | number) {
+    if (this.index_[id] === undefined) {
+      return undefined;
+    }
+
+    let current = this.index_[id].parent_;
+    while (current !== current.parent_) {
+      current = current.parent_;
+    }
+
+    return current.id_;
+  }
+
+  union(x: any, y: any) {
+    let xRoot = this.index_[this.find(x)];
+    let yRoot = this.index_[this.find(y)];
+
+    if (xRoot === undefined || yRoot === undefined || xRoot === yRoot) {
+      // x and y already belong to the same set.
+      return;
+    }
+
+    if (xRoot.rank < yRoot.rank) {
+      // Move x into the set y is a member of.
+      xRoot.parent_ = yRoot;
+    } else if (yRoot.rank_ < xRoot.rank_) {
+      // Move y into the set x is a member of.
+      yRoot.parent_ = xRoot;
+    } else {
+      // Arbitrarily choose to move y into the set x is a member of.
+      yRoot.parent_ = xRoot;
+      xRoot.rank_++;
+    }
+  };
+
+  // Returns the current number of disjoint sets.
+  size() {
+    let uniqueIndices: any = {};
+    Object.keys(this.index_).forEach((id) => {
+      uniqueIndices[id] = true;
+    });
+    return Object.keys(uniqueIndices).length;
+  };
 }
 
 function Node(this: any, id: string | number) {
@@ -15,57 +70,6 @@ function Node(this: any, id: string | number) {
   this.parent_ = this;
   this.rank_ = 0;
 }
-
-DisjointSet.prototype.makeSet = function(id: string | number) {
-  if (!this.index_[id]) {
-    const created = Node(id);
-    this.index_[id] = created;
-  }
-};
-
-DisjointSet.prototype.find = function(id: string | number) {
-  if (this.index_[id] === undefined) {
-    return undefined;
-  }
-
-  let current = this.index_[id].parent_;
-  while (current !== current.parent_) {
-    current = current.parent_;
-  }
-
-  return current.id_;
-};
-
-DisjointSet.prototype.union = function(x: any, y: any) {
-  let xRoot = this.index_[this.find(x)];
-  let yRoot = this.index_[this.find(y)];
-
-  if (xRoot === undefined || yRoot === undefined || xRoot === yRoot) {
-    // x and y already belong to the same set.
-    return;
-  }
-
-  if (xRoot.rank < yRoot.rank) {
-    // Move x into the set y is a member of.
-    xRoot.parent_ = yRoot;
-  } else if (yRoot.rank_ < xRoot.rank_) {
-    // Move y into the set x is a member of.
-    yRoot.parent_ = xRoot;
-  } else {
-    // Arbitrarily choose to move y into the set x is a member of.
-    yRoot.parent_ = xRoot;
-    xRoot.rank_++;
-  }
-};
-
-// Returns the current number of disjoint sets.
-DisjointSet.prototype.size = function() {
-  let uniqueIndices: any = {};
-  Object.keys(this.index_).forEach((id) => {
-    uniqueIndices[id] = true;
-  });
-  return Object.keys(uniqueIndices).length;
-};
 
 export function pairNodeLinks(links: any) {
   let nestedByNodes: any = {};
@@ -214,7 +218,7 @@ export function mst(graph: { nodes?: any; links?: any }) {
   let vertices = graph.nodes,
     edges = graph.links.slice(0),
     selectedEdges = [],
-    forest: any = DisjointSet();
+    forest: any = new DisjointSet();
 
   vertices.forEach((vertex: { id: any }) => {
     forest.makeSet(vertex.id);
