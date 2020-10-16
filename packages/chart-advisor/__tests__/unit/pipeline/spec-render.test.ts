703cf5bbbbaeb821fd00fddec51e138c049945ce
neoddish <neoddish@outlook.com>
2020-10-16 09:37:15 +0800

    V1 refactor (#85)
    * refactor: some optimization
    
    * refactor: some optimization
    
    * refactor: ca test dataset
    
    * test: ca pipeline
    
    * refactor: spec 2 advice

---
+++ packages/chart-advisor/__tests__/unit/pipeline/spec-render.test.ts
@@ -0,0 +1,22 @@
+import { specRender, dataToSpecs } from '../../../src';
+import { createDiv } from '../../utils/dom';
+
+describe('API - specRender', () => {
+  describe('Results', () => {
+    test('should work for basic lib config', () => {
+      const data = [
+        { city: 'London', value: 100 },
+        { city: 'Beijing', value: 200 },
+        { city: 'Shanghai', value: 600 },
+      ];
+
+      const specs = dataToSpecs(data);
+      const spec = specs[0];
+
+      const plot = specRender(createDiv(), data, spec);
+
+      // @ts-ignore
+      expect(plot.type).toBe('pie');
+    });
+  });
+});
