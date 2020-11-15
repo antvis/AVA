7eac3d55a184d6f2c2196eefdaf907b89cb67cb7
GaiaPang <40760576+GaiaPang@users.noreply.github.com>
2020-11-15 14:53:11 +0800

    feat(site): new banner image (#99)

--- packages/ava-site/site/pages/index.zh.tsx
+++ packages/ava-site/site/pages/index.zh.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import SEO from '@antv/gatsby-theme-antv/site/components/Seo';
 import { useTranslation } from 'react-i18next';
 import Banner from '@antv/gatsby-theme-antv/site/components/Banner';
-import BannerSVG from '@antv/gatsby-theme-antv/site/components/BannerSVG';
 
 const IndexPage = () => {
   const { t, i18n } = useTranslation();
@@ -19,13 +18,18 @@ const IndexPage = () => {
     },
   ];
 
-  // const coverImage = <img width='100%' src='https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*o40BRo-ANLoAAAAAAAAAAABkARQnAQ' alt='cover' />
+  const coverImage = (
+    <img
+      width="100%"
+      src="https://gw.alipayobjects.com/zos/bmw-prod/e11f026f-325b-4e71-8613-8297febcfdd0.svg"
+      alt="cover"
+    />
+  );
   return (
     <>
       <SEO title={t('蚂蚁数据可视化')} lang={i18n.language} />
       <Banner
-        // coverImage={coverImage}
-        coverImage={<BannerSVG />}
+        coverImage={coverImage}
         title={t('AVA')}
         description={t(
           'AVA 是为了更简便的可视分析而生的技术框架。 VA 代表可视分析（Visual Analytics），而第一个 A 具有多重涵义：其目标是成为一个自动化（Automated）、智能驱动（AI driven）、支持增强分析（Augmented）的可视分析解决方案。'
