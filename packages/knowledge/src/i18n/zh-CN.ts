84f0e9969f30c3f58951b2a746b90d4418d418b1
Sun Fuling <30462555+soundquiet@users.noreply.github.com>
2020-11-15 18:23:51 +0800

    feat(CKB): add radial bar chart spec (#98)
    * feat(CKB): add radial bar chart spec
    
    * Apply suggestions from code review
    
    Co-authored-by: neoddish <neoddish@outlook.com>
    
    * update radialbarchart spec & test case
    
    Co-authored-by: neoddish <neoddish@outlook.com>

--- packages/knowledge/src/i18n/zh-CN.ts
+++ packages/knowledge/src/i18n/zh-CN.ts
@@ -210,9 +210,9 @@ export const zhCN: TranslateList = {
     },
 
     radial_bar_chart: {
-      name: '',
-      alias: [],
-      def: 'TBD',
+      name: '径向条形图',
+      alias: ['环形条形图', '环形柱状图', '玉珏图'],
+      def: '径向条形图是一种在极坐标系下绘制的条形图。径向条形图沿半径方向展示分类维度，使用角度来反应数值。',
     },
 
     mirror_bar_chart: {
