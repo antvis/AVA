ffe46f1242fab1440f364fc1e8cceb39e3525dd1
羽熙 <1491812683@qq.com>
2021-05-18 16:10:34 +0800

    fix(chart-advisor): remove redundant types file

--- packages/chart-advisor/src/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-// cover @antv/data-set unTypes modules
-declare module 'd3-geo' {
-  // @ts-ignore
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  class ExtendedFeature {}
-}
-
-declare module 'd3-sankey';
