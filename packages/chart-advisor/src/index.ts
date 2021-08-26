2534806816ee8e498b12d6c306e25a7e2e90447e
jiazhewang <neoddish@outlook.com>
2021-08-26 20:59:27 +0800

    fix(CA): rm import pkg and expand size limit

--- packages/chart-advisor/src/index.ts
+++ packages/chart-advisor/src/index.ts
@@ -1,6 +1,3 @@
-import pkg from '../package.json';
-
-export const { version } = pkg;
 export { Advisor } from './advisor';
 export { Linter } from './linter';
 export { ChartAdvisor } from './chart-advisor';
