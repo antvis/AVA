46f390fb779e34f28bfd79bf058526a8edf2d2d8
soundquiet <625941917@qq.com>
2021-08-26 18:01:31 +0800

    feat: ca export

--- packages/chart-advisor/src/index.ts
+++ packages/chart-advisor/src/index.ts
@@ -1,3 +1,6 @@
 import pkg from '../package.json';
 
 export const { version } = pkg;
+export { Advisor } from './advisor';
+export { Linter } from './linter';
+export { ChartAdvisor } from './chart-advisor';
