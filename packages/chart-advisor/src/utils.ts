ef64043d7f3f5558c4c2f3c5d8d1bf136958a770
soundquiet <625941917@qq.com>
2021-08-31 18:37:58 +0800

    fix: replace destructuring by delete

--- packages/chart-advisor/src/utils.ts
+++ packages/chart-advisor/src/utils.ts
@@ -5,9 +5,3 @@ export function hasSubset(array1: any[], array2: any[]): boolean {
 export function intersects(array1: any[], array2: any[]): boolean {
   return array2.some((e) => array1.includes(e));
 }
-
-export function deleteProperty(source: Record<string, any>, property: string) {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { [property]: _, ...rest } = source;
-  return rest;
-}
