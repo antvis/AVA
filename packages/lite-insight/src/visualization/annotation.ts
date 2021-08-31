677e634bf85e39cdff540b83aba37e381bf9de17
cxxxxxn <christy05.chen@gmail.com>
2021-08-31 18:31:37 +0800

    feat(lite-insight): update annotation

--- packages/lite-insight/src/visualization/annotation.ts
+++ packages/lite-insight/src/visualization/annotation.ts
@@ -1,25 +1,58 @@
 import { PatternInfo, PointPatternInfo, InsightInfo, TrendInfo } from '../interface';
-import { insightNameMap } from './description';
+import { dataFormat } from './util';
+
+const COLOR: Record<string, string> = {
+  highlight: '#E09322',
+  outlier: '#CB5140',
+  font: '#2C3542',
+};
+
+const TEXT_STYLE = {
+  textAlign: 'center',
+  fill: COLOR.font,
+  opacity: 0.65,
+};
+const BOLD = 500;
+
+interface Text {
+  content: number | string;
+  style?: Record<string, number | string>;
+}
+
+const annotationText = (
+  texts: Text[],
+  position: [number | string, number | string],
+  offsetY: number = 0
+) => {
+  return texts.map((text, i) => ({
+    type: 'text',
+    content: dataFormat(text.content),
+    position,
+    offsetY: offsetY + i * 15,
+    style: {
+      ...TEXT_STYLE,
+      ...text.style
+    }
+  }));
+};
 
 const generateAnnotationConfigItem = (pattern: PatternInfo, insightInfo: InsightInfo) => {
   const { breakdowns, measures, data } = insightInfo;
-  if (['category_outlier', 'change_point', 'time_series_outlier'].includes(pattern.type)) {
+  if (['change_point', 'time_series_outlier'].includes(pattern.type)) {
     const patternInfo = pattern as PointPatternInfo;
     const { index } = patternInfo;
+    const color = pattern.type === 'time_series_outlier' ? COLOR.outlier : COLOR.highlight;
+    const xValue = data[index][breakdowns[0]];
+    const yValue = data[index][measures[0].field];
+
     return [
       {
         type: 'dataMarker',
-        position: [data[index][breakdowns[0]], data[index][measures[0].field]],
+        position: [xValue, yValue],
         point: {
           style: {
-            fill: '#f5222d',
-            stroke: '#f5222d',
-          },
-        },
-        text: {
-          content: insightNameMap[pattern.type],
-          style: {
-            textAlign: 'left',
+            fill: '#fff',
+            stroke: color,
           },
         },
         line: {
@@ -27,6 +60,44 @@ const generateAnnotationConfigItem = (pattern: PatternInfo, insightInfo: Insight
         },
         autoAdjust: false,
       },
+      ...annotationText([{
+        content: xValue
+      }, {
+        content: yValue,
+        style: {
+          fontWeight: BOLD,
+        }
+      }], [xValue, yValue], -42),
+    ];
+  }
+  if (pattern.type === 'category_outlier') {
+    const patternInfo = pattern as PointPatternInfo;
+    const { index } = patternInfo;
+    const xValue = data[index][breakdowns[0]];
+    const yValue = data[index][measures[0].field];
+    return [
+      {
+        type: 'regionFilter',
+        start: (xScale: any) => {
+          const ratio = xScale.ticks ? 1 / xScale.ticks.length : 1;
+          const x = xScale.scale(xValue) - ratio / 2;
+          return [`${x * 100}%`, '0%'];
+        },
+        end: (xScale: any) => {
+          const ratio = xScale.ticks ? 1 / xScale.ticks.length : 1;
+          const x = xScale.scale(xValue) + ratio / 2;
+          return [`${x * 100}%`, '100%'];
+        },
+        color: COLOR.outlier,
+      },
+      ...annotationText([{
+        content: xValue
+      }, {
+        content: yValue,
+        style: {
+          fontWeight: BOLD,
+        }
+      }], [xValue, yValue], -22),
     ];
   }
   if (pattern.type === 'trend') {
@@ -40,16 +111,8 @@ const generateAnnotationConfigItem = (pattern: PatternInfo, insightInfo: Insight
         start: ['min', points[0]],
         end: ['max', points[points.length - 1]],
         style: {
-          lineDash: [4, 4],
-          stroke: 'orange',
-        },
-        text: {
-          content: 'trend',
-          position: 'right',
-          offsetY: 18,
-          style: {
-            textAlign: 'right',
-          },
+          lineDash: [2, 2],
+          stroke: COLOR.highlight,
         },
       },
     ];
--- packages/lite-insight/src/visualization/util.ts
+++ packages/lite-insight/src/visualization/util.ts
@@ -14,3 +14,26 @@ export const join = (items: (string | number)[]) => {
 export const getDatumPositionString = (datum: Datum, dimension: string, measure: string) => {
   return `(${datum[dimension]}, ${datum[measure]})`;
 };
+
+// data format.
+// dataFormat(12, 1) = 12
+// dataFormat(1234, 1) = 1.2k
+// dataFormat(123.456, 2) = 123.46
+export const dataFormat = (value: number | string, digits: number = 2) => {
+  if(typeof value === 'string') return value;
+
+  const formatMap = [
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "k" },
+    { value: 1e6, symbol: "M" },
+    { value: 1e9, symbol: "G" },
+    { value: 1e12, symbol: "T" },
+    { value: 1e15, symbol: "P" },
+    { value: 1e18, symbol: "E" }
+  ];
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  var item = formatMap.slice().reverse().find(function(item) {
+    return value >= item.value;
+  });
+  return item ? (value / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
+};
