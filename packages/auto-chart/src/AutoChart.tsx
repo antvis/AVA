2f5791fd41f59d5f56269fb186d293185180aeeb
beewolf233 <1710615975@qq.com>
2021-09-06 16:16:24 +0800

    chore(auto-chart): add basic auto-chart package and demo use it

---
+++ packages/auto-chart/src/AutoChart.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+interface Props {
+  id: string;
+  className?: string;
+  width?: number;
+  height?: number;
+  data?: any[];
+  toolbar?:boolean; 
+  development?: boolean;
+  title?: string;
+  description?: string;
+  purpose?: 'Comparison' | 'Trend' | 'Distribution' | 'Rank' | 'Proportion' | 'Composition';
+};
+
+export const AutoChart = (props: Props) => {
+  return (
+    <div id={props.id}>
+      {props.title}
+    </div>
+  );
+};
---
+++ packages/auto-chart/src/index.tsx
@@ -0,0 +1,1 @@
+export { AutoChart } from './AutoChart';
