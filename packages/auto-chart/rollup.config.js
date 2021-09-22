8485d0ffe5b5a7657efde791d4fa0d35d15bb422
beewolf233 <1710615975@qq.com>
2021-09-22 12:13:44 +0800

    fix(auto-chart): add auto-chart build set

--- packages/auto-chart/rollup.config.js
+++ packages/auto-chart/rollup.config.js
@@ -1,3 +1,5 @@
+import postcss from 'rollup-plugin-postcss';
+import autoprefixer from 'autoprefixer';
 import rollupConfig from '../../rollup.config';
 
 export default rollupConfig(
@@ -11,4 +13,28 @@ export default rollupConfig(
       },
     }
   },
+  [postcss({
+    // Extract CSS to the same location where JS file is generated but with .css extension.
+    extract: true,
+    // Use named exports alongside default export.
+    namedExports: true,
+    // Minimize CSS, boolean or options for cssnano.
+    minimize: true,
+    // Enable sourceMap.
+    sourceMap: true,
+    // This plugin will process files ending with these extensions and the extensions supported by custom loaders.
+    extensions: ['.less', '.css'],
+    plugins: [
+      autoprefixer({
+        overrideBrowserslist: [
+          'defaults',
+          'not ie < 11',
+          'last 2 versions',
+          '> 1%',
+          'iOS 7',
+          'last 3 iOS versions'
+        ]
+      })
+    ]
+  })]
 );
