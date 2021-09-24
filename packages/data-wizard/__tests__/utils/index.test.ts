e1f9b4e3b16956a3890c30662883801951cf14d2
pddpd <pddpengdi@gmail.com>
2021-09-24 10:53:50 +0800

    feat(DW): improve utils tests

--- packages/data-wizard/__tests__/utils/index.test.ts
+++ packages/data-wizard/__tests__/utils/index.test.ts
@@ -1,9 +1,78 @@
-import { isBoolean } from '../../src/utils';
+import * as utils from '../../src/utils';
+
+test('isNull', () => {
+  const data = 'null';
+  expect(utils.isNull(data)).toBe(true);
+});
+
+test('isString', () => {
+  const data = 'str';
+  expect(utils.isString(data)).toBe(true);
+});
+
+
+test('isNumber', () => {
+  const data = 201;
+  expect(utils.isNumber(data)).toBe(true);
+});
+
+test('isDigit', () => {
+  const data = '5.2';
+  expect(utils.isDigit(data)).toBe(true);
+});
+
+test('isInteger', () => {
+  const data1 = 1;
+  expect(utils.isInteger(data1)).toBe(true);
+
+  const data2 = '1';
+  expect(utils.isInteger(data2, true)).toBe(true);
+});
+
+test('isFloat', () => {
+  const data1 = 1.2;
+  expect(utils.isFloat(data1)).toBe(true);
+
+  const data2 = '1.2';
+  expect(utils.isFloat(data2, true)).toBe(true);
+});
+
+test('isDate', () => {
+  const data1 = new Date();
+  expect(utils.isDate(data1)).toBe(true);
+
+  const data2 = '2021-09-24';
+  expect(utils.isDate(data2, true)).toBe(true);
+});
 
 test('isBoolean', () => {
   const data1 = true;
-  expect(isBoolean(data1)).toBe(true);
+  expect(utils.isBoolean(data1)).toBe(true);
 
   const data2 = ['是', '否'];
-  expect(isBoolean(data2, true)).toBe(true);
+  expect(utils.isBoolean(data2, true)).toBe(true);
+});
+
+test('isObject', () => {
+  const data = {a: 1};
+  expect(utils.isObject(data)).toBe(true);
+});
+
+test('isArray', () => {
+  const data = [1, 2, 3];
+  expect(utils.isArray(data)).toBe(true);
+});
+
+test('unique', () => {
+  const data = [1, 2, 3, 3, 2, 1];
+  expect(utils.unique(data)).toStrictEqual([1, 2, 3]);
+});
+
+test('range', () => {
+  const data = 4;
+  expect(utils.range(data)).toStrictEqual([0, 1, 2, 3]);
+});
+
+test('assert', () => {
+  expect(() => utils.assert(false, 'It is false!')).toThrow('It is false!');
 });
