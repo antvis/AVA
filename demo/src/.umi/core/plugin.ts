86ea61baad3e5af15601f053c395072ac6b2d7ac
bbsqq <1491812683@qq.com>
2021-11-20 01:16:28 +0800

    chore: add git ignore

--- demo/src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/yuxi/Desktop/AVA/demo/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','getInitialState','initialStateConfig','request',],
-});
-
-export { plugin };
