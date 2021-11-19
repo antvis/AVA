e64bef5f6579c582d8a399253a48cdf87e10a1e9
bbsqq <1491812683@qq.com>
2021-11-19 23:28:27 +0800

    refactor(demo): use umi as project cli

---
+++ demo/.umirc.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    { path: '/', component: '@/pages/index' },
+  ],
+  fastRefresh: {},
+  workerLoader: {},
+});
