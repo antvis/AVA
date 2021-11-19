c5e154949651979f624e1d08733d0a65621b6f74
bbsqq <1491812683@qq.com>
2021-11-19 23:28:27 +0800

    refactor(demo): use umi as project cli

---
+++ demo/typings.d.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+declare module '*.css';
+declare module '*.less';
+declare module '*.png';
+declare module '*.svg' {
+  export function ReactComponent(props: React.SVGProps<SVGSVGElement>): React.ReactElement;
+  const url: string;
+  export default url;
+}
