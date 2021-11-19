11a16dc4385ae703b73b921fcb958af2407b85be
bbsqq <1491812683@qq.com>
2021-11-20 00:21:06 +0800

    fix(auto-chart): mock panel stack overflow

--- demo/src/demos/AutoChart/index.tsx
+++ demo/src/demos/AutoChart/index.tsx
@@ -8,7 +8,7 @@ const autoChartExp = (
     <h1>case 1</h1>
     <AutoChart title="CASE 1" description="auto chart analysis" data={testData} language={'zh-CN'} />
     <h1>case 2</h1>
-    <AutoChart title="CASE 2" description="no data can mock chart" data={[]} language={'zh-CN'} />
+    <AutoChart title="CASE 2" description="no data can mock chart" language={'zh-CN'} />
   </div>
 );
 
