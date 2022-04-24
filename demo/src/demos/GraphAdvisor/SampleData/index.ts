19afc5127cab7734f94aa32f24e39b6d6eb1dc74
neoddish <neoddish@outlook.com>
2022-04-25 00:29:04 +0800

    style: fix prettier errors

--- demo/src/demos/GraphAdvisor/SampleData/index.ts
+++ demo/src/demos/GraphAdvisor/SampleData/index.ts
@@ -5,9 +5,9 @@ import { SubgraphData } from './Subgraph';
 import { EuroCreditData } from './table';
 
 export const ExampleDataset = [
-  {id: 'flowgraph', name: 'Flow Graph', data: FlowGraphData},
-  {id: 'table', name: 'Euro Credit', data: EuroCreditData},
-  {id: 'sankey', name: 'Sankey', data: SankeyGraphData},
-  {id: 'tree', name: 'Knowledge Tree', data: KnowledgeTreeData},
-  {id: 'subgraphs', name: 'Subgraphs', data: SubgraphData},
+  { id: 'flowgraph', name: 'Flow Graph', data: FlowGraphData },
+  { id: 'table', name: 'Euro Credit', data: EuroCreditData },
+  { id: 'sankey', name: 'Sankey', data: SankeyGraphData },
+  { id: 'tree', name: 'Knowledge Tree', data: KnowledgeTreeData },
+  { id: 'subgraphs', name: 'Subgraphs', data: SubgraphData },
 ];
