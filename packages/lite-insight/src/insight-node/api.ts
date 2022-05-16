fbe486b2be5f4bf052da39505f18f8d4c244c1b4
Esorakouki <tygyh2012@gmail.com>
2022-05-16 14:45:15 +0800

    docs(li): add @deprecated annotation for async api

--- packages/lite-insight/src/insight-node/api.ts
+++ packages/lite-insight/src/insight-node/api.ts
@@ -16,6 +16,9 @@ export const getDataInsights = (sourceData: Datum[], options?: InsightOptions): 
   return extractResult;
 };
 
+/**
+ * @deprecated This function will be removed in 3.0.0 because web workers inside a package has a low compatibility with bundlers.
+ */
 export const getDataInsightsAsync = async (sourceData: Datum[], options?: InsightOptions): Promise<InsightsResult> => {
   const insight = await spawn(new Worker('./worker'));
   const extractResult = await insight.extractInsights(sourceData, options);
