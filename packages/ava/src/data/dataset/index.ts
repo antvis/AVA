d8d36dc84783f5170019280f70a7d94e76bcea57
pddpd <pddpengdi@gmail.com>
2022-11-15 14:44:34 +0800

    refactor(ava/data): migrate dataset files

--- packages/ava/src/data/dataset/index.ts
+++ packages/ava/src/data/dataset/index.ts
@@ -1,1 +1,6 @@
-// export { dataset } from './dataset';
+import Series from './series';
+import DataFrame from './data-frame';
+import GraphData from './graph-data';
+
+export { Series, DataFrame, GraphData };
+export * from './types';
