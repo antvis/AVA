dc1adcaa1699d46b8474979fead667aa87436a87
bbsqq <1491812683@qq.com>
2022-11-17 14:30:48 +0800

    fix(ava-react/ntv): add avar- classname prefix

--- packages/ava-react/src/NarrativeTextVis/constants.ts
+++ packages/ava-react/src/NarrativeTextVis/constants.ts
@@ -1,1 +1,7 @@
-export const NTV_CONTAINER_CLS = 'ntv-container';
+import { getPrefixCls } from '../utils';
+
+/** ntv comp className */
+export const NTV_PREFIX_CLS = getPrefixCls('ntv'); // return 'avar-ntv'
+
+/** export img alt attr */
+export const NTV_IMG_ALT = NTV_PREFIX_CLS;
