7067c9c651d6fe91a7a6979e08783af6ceffbceb
pddpd <pddpengdi@gmail.com>
2022-11-17 21:40:12 +0800

    refactor(ava/data): remove data-wizard files

--- packages/data-wizard/jest.config.js
+++
@@ -1,7 +0,0 @@
-const base = require('../../jest.config.base.js');
-
-module.exports = {
-  ...base,
-  name: 'data-wizard',
-  displayName: 'data-wizard',
-};
--- packages/data-wizard/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * as analyzer from './analyzer';
-export * as statistics from './statistics';
-export * as utils from './utils';
-export * as random from './random';
-export { Series, DataFrame, GraphData } from './dataset';
