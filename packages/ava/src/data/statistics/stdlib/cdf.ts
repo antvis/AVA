869254ebb48389bcef66a3f29f6e94a8bbae0833
laixingui.lxg <laixingui1608@163.com>
2023-03-13 11:37:55 +0800

    fix(ava/data): add notes and document links

--- packages/ava/src/data/statistics/stdlib/cdf.ts
+++ packages/ava/src/data/statistics/stdlib/cdf.ts
@@ -2,7 +2,7 @@ import { isNil } from 'lodash';
 
 /**
  * Evaluates the cumulative distribution function (CDF) for a normal distribution at a value x
- * - Reference to equation 26.2.17 in Abramowitz and Stegun: Handbook of Mathematical Functions
+ * - Reference to equation 26.2.17 in https://personal.math.ubc.ca/~cbm/aands/abramowitz_and_stegun.pdf
  * @param mu mean
  * @param sigma standard deviation
  * */
@@ -12,9 +12,16 @@ export const cdf = (x: number, mu: number = 0, sigma: number = 1): number => {
   const normalX = (x - mu) / sigma;
   /** probability density function of the standard normal distribution */
   const Zx = (1 / Math.sqrt(2 * Math.PI)) * Math.exp((-1 * normalX ** 2) / 2);
+  /**
+   * - use approximate elementary functional algorithm, error less than 4.5e-4
+   * @todo add document explaining the derivation process
+   * */
   const t = 1 / (1 + 0.33267 * normalX);
   // error less than 1e-5
   const Px = 1 - Zx * (0.4361836 * t - 0.1201676 * t ** 2 + 0.937298 * t ** 3);
-  // consistent with the number of digits in @stdlib/stats/base/dists/normal
+  /**
+   * consistent with the number of digits in @stdlib/stats/base/dists/normal
+   * @todo fix precision issue
+   */
   return Number(Px.toFixed(3));
 };
