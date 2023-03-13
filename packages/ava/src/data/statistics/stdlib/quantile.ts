64c942c1b9bdf946b4f1591a3dc1131ff97fe139
laixingui.lxg <laixingui1608@163.com>
2023-03-13 11:37:55 +0800

    fix(ava/data): add notes and document links

--- packages/ava/src/data/statistics/stdlib/quantile.ts
+++ packages/ava/src/data/statistics/stdlib/quantile.ts
@@ -1,6 +1,6 @@
 /**
  * Normal distribution quantile function
- * - Reference to equation 26.2.23 in Abramowitz and Stegun: Handbook of Mathematical Functions
+ * - Reference to equation 26.2.23 in https://personal.math.ubc.ca/~cbm/aands/abramowitz_and_stegun.pdf
  * - If F(x) is the cumulative distribution function of a normal distribution N(mu, sigma), F(x) = p, quantile(p, mu, sigma) = x.
  * - The value range of p is [0, 1]. If p > 1 or p < 0, return NaN.
  * @param p input value
@@ -12,13 +12,20 @@ export const normalDistributionQuantile = (p: number, mu: number = 0, sigma: num
   // formula requires that p is no more than 0.5
   const adjustedP = p <= 0.5 ? p : 1 - p;
   const t = Math.sqrt(-2 * Math.log(adjustedP));
-  // Q(qXp) + F(qXp) = 1, error less than 4.5e-4
+  /**
+   * Q(qXp) + F(qXp) = 1
+   * - use approximate elementary functional algorithm, error less than 4.5e-4
+   * @todo add document explaining the derivation process
+   * */
   const qXp =
     t - (2.515517 + 0.802853 * t + 0.010328 * t ** 2) / (1 + 1.432788 * t + 0.189269 * t ** 2 + 0.001308 * t ** 3);
   // quantile of standard normal distribution
   const normalXp = p <= 0.5 ? -1 * qXp : qXp;
   // transfer to quantile of normal distribution N(mu, sigma)
   const Xp = sigma * normalXp + mu;
-  // consistent with the number of digits in @stdlib/stats/base/dists/normal
+  /**
+   * consistent with the number of digits in @stdlib/stats/base/dists/normal
+   * @todo fix precision issue
+   */
   return Number(Xp.toFixed(3));
 };
