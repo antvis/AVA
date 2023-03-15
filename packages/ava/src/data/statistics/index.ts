871e001446d6ace773bf31a1be480bc5da9d366d
laixingui.lxg <laixingui1608@163.com>
2023-03-15 11:11:57 +0800

    fix(ava/data): export methods in statistics

---
+++ packages/ava/src/data/statistics/index.ts
@@ -0,0 +1,7 @@
+export * from './base';
+export { cdf } from './cdf';
+export { maxabs } from './maxabs';
+export * from './quantile';
+export * from './lowess';
+export * from './vector';
+export { pcorrtest } from './pcorrtest';
