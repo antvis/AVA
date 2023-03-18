6938112d1a84b100b6fd56ccf1d6e797c81fa3d8
ixuxinyue <xuxinyue.xxy@antgroup.com>
2023-03-18 12:33:48 +0800

    feat: update chart render in playground

--- playground/src/DevPlayground/ChartAdvisor/Chart.tsx
+++ playground/src/DevPlayground/ChartAdvisor/Chart.tsx
@@ -1,33 +1,27 @@
 import { useEffect, useRef } from 'react';
 
-import { G2Spec, render } from '@antv/g2';
+import { render } from '@antv/g2';
 
 export const Chart = ({ id, spec }: any) => {
   const containerRef = useRef<HTMLElement>(null);
   useEffect(() => {
     if (containerRef.current) {
       const container = document.getElementById('container');
-      const style = getComputedStyle(container);
-      const demoSpec: G2Spec = {
-        width: parseInt(style.width),
-        height: parseInt(style.height),
-        type: 'interval',
-        data: [
-          { genre: 'Sports', sold: 275 },
-          { genre: 'Strategy', sold: 115 },
-          { genre: 'Action', sold: 120 },
-          { genre: 'Shooter', sold: 350 },
-          { genre: 'Other', sold: 150 },
-        ],
-        encode: {
-          x: 'genre',
-          y: 'sold',
-        },
-      };
-      const node = render(demoSpec);
+      const style = container ? getComputedStyle(container) : undefined;
+      const size = style
+        ? {
+            width: parseInt(style.width, 10),
+            height: parseInt(style.height, 10),
+          }
+        : {};
+      const node = render({
+        ...size,
+        ...spec,
+        // theme:'classic'
+      });
       containerRef.current.appendChild(node);
     }
   }, []);
-
+  // @ts-ignore 待 g2 确认渲染方式
   return <div ref={container} id={id} style={{ width: '100%', height: 200, margin: 'auto' }}></div>;
 };
