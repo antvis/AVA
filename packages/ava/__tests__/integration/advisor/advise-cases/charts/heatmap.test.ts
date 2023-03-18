a1bba1efb99ca82a1634307e8b2637fea2a33156
GuangMingYouBei <hz2149@nyu.edu>
2023-03-18 17:05:05 +0800

    test(advisor): revise test case of heatmap

--- packages/ava/__tests__/integration/advisor/advise-cases/charts/heatmap.test.ts
+++ packages/ava/__tests__/integration/advisor/advise-cases/charts/heatmap.test.ts
@@ -7,9 +7,30 @@ describe('should advise line', () => {
   test('test case from @antv/data-samples', async () => {
     const data = await dataByChartId('heatmap');
 
+    /*
+     这个数据是这样的：[
+      { "x": -5, "y": -5, "z": 50 },
+      { "x": -4, "y": -5, "z": 41 },
+      { "x": -3, "y": -5, "z": 34 },
+      { "x": -2, "y": -5, "z": 29 },
+      { "x": -1, "y": -5, "z": 26 },
+      { "x": 0, "y": -5, "z": 25 },
+      { "x": 1, "y": -5, "z": 26 },
+    ...]
+      会导致 dw 自动将 x，y 判断为 interval 类型。
+      会导致推荐出错，因此在推荐前，将 x，y 强行指定为 nominal 类型。
+     */
+
     const myAdvisor = new Advisor();
-    const advices = myAdvisor.advise({ data });
-    // TODO heatmap is invalid in advisor for unknown reason
+    const advices = myAdvisor.advise({
+      data,
+      // 传 dataprops 的原因 参考上方注释
+      dataProps: [
+        { name: 'x', levelOfMeasurements: ['Nominal'] },
+        { name: 'y', levelOfMeasurements: ['Nominal'] },
+      ],
+    });
+
     expect(advices.map((advice) => advice.type).includes('heatmap')).toBe(true);
   });
 });
