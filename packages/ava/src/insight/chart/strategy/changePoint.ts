16dff17206de17280354e51b4069aa906089f987
pddpd <pddpengdi@gmail.com>
2023-03-19 15:32:30 +0800

    fix(ava/insight): generate marks and wrap view spec

--- packages/ava/src/insight/chart/strategy/changePoint.ts
+++ packages/ava/src/insight/chart/strategy/changePoint.ts
@@ -1,11 +1,11 @@
-import { G2Spec, Mark } from '@antv/g2';
+import { Mark } from '@antv/g2';
 
 import { ChangePointInfo, InsightInfo } from '../../types';
 import { INSIGHT_COLOR_PLATTE } from '../constants';
 import { dataFormat } from '../../../utils';
 
-import { pointMarkStrategy } from './augmentedMarks/pointMark';
-import { textMarkStrategy } from './augmentedMarks/textMark';
+import { pointMarkStrategy } from './commonMarks/pointMark';
+import { textMarkStrategy } from './commonMarks/textMark';
 import { insight2ChartStrategy } from './chartStrategy';
 
 export const changePointAugmentedMarksStrategy = (patterns: ChangePointInfo[]): Mark[] => {
@@ -15,11 +15,8 @@ export const changePointAugmentedMarksStrategy = (patterns: ChangePointInfo[]): 
   return [pointMark, textMark];
 };
 
-export const changePointStrategy = (insight: InsightInfo<ChangePointInfo>, patterns: ChangePointInfo[]): G2Spec => {
+export const changePointStrategy = (insight: InsightInfo<ChangePointInfo>, patterns: ChangePointInfo[]): Mark[] => {
   const chart = insight2ChartStrategy(insight);
   const augmentedMarks = changePointAugmentedMarksStrategy(patterns);
-  return {
-    ...chart,
-    ...augmentedMarks,
-  };
+  return [chart, ...augmentedMarks];
 };
