7004a94cef80572c11ce50988f3bb4de8ecb2bc6
pddpd <pddpengdi@gmail.com>
2023-03-19 11:22:45 +0800

    fix(ava/insight): adjust timeSeriesOutlier related parameters

--- packages/ava/src/insight/insights/util.ts
+++ packages/ava/src/insight/insights/util.ts
@@ -2,9 +2,15 @@ import { mean } from 'lodash';
 
 import { standardDeviation, cdf } from '../../data';
 
-export const calculatePValue = (values: number[], target: number) => {
+export const calculatePValue = (
+  values: number[],
+  target: number,
+  alternative: 'two-sided' | 'less' | 'greater' = 'greater'
+) => {
   const meanValue = mean(values);
   const std = standardDeviation(values);
-  const pValue = 1 - cdf(target, meanValue, std);
-  return pValue;
+  const cdfValue = cdf(target, meanValue, std);
+  if (alternative === 'two-sided') return cdfValue < 0.5 ? cdfValue : 1 - cdfValue;
+  if (alternative === 'less') return cdfValue;
+  return 1 - cdfValue;
 };
