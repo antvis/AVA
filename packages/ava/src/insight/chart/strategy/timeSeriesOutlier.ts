c61910fd4f3caa783e43e97bfe74e3cecac9f9a4
pddpd <pddpengdi@gmail.com>
2023-03-19 16:04:25 +0800

    feat(ava/insight): add timeSeriesOutlier mark strategy

--- packages/ava/src/insight/chart/strategy/timeSeriesOutlier.ts
+++ packages/ava/src/insight/chart/strategy/timeSeriesOutlier.ts
@@ -4,11 +4,91 @@ import { TimeSeriesOutlierInfo, InsightInfo } from '../../types';
 
 import { insight2ChartStrategy } from './chartStrategy';
 
-export const timeSeriesOutlierStrategyAugmentedMarksStrategy = (patterns: TimeSeriesOutlierInfo[]): Mark[] => {
-  // const color = INSIGHT_COLOR_PLATTE.highlight;
-  // const pointMark = pointMarkStrategy(patterns, { strokeColor: color });
-  // const textMark = textMarkStrategy(patterns, { formatter: dataFormat });
-  // return [pointMark, textMark];
+const BASELINE = 'baseline';
+const INTERVAL = 'interval';
+const OUTLIER = 'outlier';
+
+export const timeSeriesOutlierStrategyAugmentedMarksStrategy = (
+  insight: InsightInfo<TimeSeriesOutlierInfo>,
+  patterns: TimeSeriesOutlierInfo[]
+): Mark[] => {
+  const { baselines, thresholds } = patterns[0];
+  const outlierIndexes = patterns.map((pattern) => pattern.index);
+  const {
+    data: chartData,
+    dimensions: [{ fieldName: dimensionName }],
+    measures: [{ fieldName: measureName }],
+  } = insight;
+  const data = chartData.map((datum, index) => {
+    const baseline = baselines[index];
+    const interval = [baseline - Math.abs(thresholds[0]), baseline + thresholds[1]];
+    return {
+      [dimensionName]: datum[dimensionName],
+      baseline,
+      interval,
+      ...(outlierIndexes.includes(index) ? { [OUTLIER]: datum[measureName] } : undefined),
+    };
+  });
+
+  const baselineMark: Mark = {
+    type: 'line',
+    data,
+    encode: {
+      x: dimensionName,
+      y: BASELINE,
+    },
+    style: {
+      lineWidth: 2,
+      stroke: '#ffa45c',
+    },
+    tooltip: {
+      title: '',
+      items: [{ name: BASELINE, channel: 'y' }],
+    },
+  };
+
+  const intervalMark: Mark = {
+    type: 'area',
+    data,
+    encode: {
+      x: dimensionName,
+      y: INTERVAL,
+    },
+    style: {
+      fillOpacity: 0.3,
+      fill: '#ffd8b8',
+    },
+    tooltip: {
+      title: '',
+      items: [
+        (d, i, data, column) => ({
+          name: INTERVAL,
+          value: `${column.y.value[i as number]}-${column.y1.value[i as number]}`,
+          color: '#ffd8b8',
+        }),
+      ],
+    },
+  };
+
+  const outlierMark: Mark = {
+    type: 'point',
+    data,
+    encode: {
+      x: dimensionName,
+      y: OUTLIER,
+      shape: 'point',
+      size: 3,
+    },
+    style: {
+      fill: '#f4664a',
+    },
+    tooltip: {
+      title: '',
+      items: [{ name: OUTLIER, channel: 'y' }],
+    },
+  };
+
+  return [baselineMark, intervalMark, outlierMark];
 };
 
 export const timeSeriesOutlierStrategy = (
@@ -16,6 +96,6 @@ export const timeSeriesOutlierStrategy = (
   patterns: TimeSeriesOutlierInfo[]
 ): Mark[] => {
   const chartMark = insight2ChartStrategy(insight);
-  const augmentedMarks = timeSeriesOutlierStrategyAugmentedMarksStrategy(patterns);
+  const augmentedMarks = timeSeriesOutlierStrategyAugmentedMarksStrategy(insight, patterns);
   return [chartMark, ...augmentedMarks];
 };
