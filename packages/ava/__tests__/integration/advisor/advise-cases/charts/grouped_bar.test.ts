864b411ed04aed388daf540a7ee2876de74d7e41
GuangMingYouBei <99389512+GuangMingYouBei@users.noreply.github.com>
2023-03-20 23:42:40 +0800

    chore: remove invalid TODOs (#606)

--- packages/ava/__tests__/integration/advisor/advise-cases/charts/grouped_bar.test.ts
+++ packages/ava/__tests__/integration/advisor/advise-cases/charts/grouped_bar.test.ts
@@ -5,8 +5,6 @@ import { Advisor } from '../../../../../src/advisor/index';
 // In the following cases, the recommended result should be a grouped_bar chart.
 describe('should advise grouped_bar', () => {
   test('test case from @antv/data-samples', async () => {
-    // TO DO  grouped_bar_chart dataset can not be used for now, because time field will lead to line chart
-    // const data = await dataByChartId('grouped_bar_chart');
     const data = await dataByChartId('stacked_bar_chart');
     const myAdvisor = new Advisor();
     const advices = myAdvisor.advise({ data });
@@ -18,7 +16,6 @@ describe('should advise grouped_bar', () => {
 describe('should NOT advise bar/bar', () => {
   test('categrorical field should not be x-axis of bar chart', async () => {
     const data = await dataByChartId('line_chart');
-
     const myAdvisor = new Advisor();
     const advices = myAdvisor.advise({ data });
     expect(advices[0].type === 'grouped_bar_chart').toBe(false);
--- packages/ava/__tests__/integration/advisor/advise-cases/charts/percent_stacked_bar.test.ts
+++ packages/ava/__tests__/integration/advisor/advise-cases/charts/percent_stacked_bar.test.ts
@@ -5,8 +5,6 @@ import { Advisor } from '../../../../../src/advisor/index';
 // In the following cases, the recommended result should be a percent_stacked_bar chart.
 describe('should advise percent_stacked_bar', () => {
   test('test case from @antv/data-samples', async () => {
-    // TO DO  percent_stacked_bar_chart dataset can not be used for now, because time field will lead to line chart
-    // const data = await dataByChartId('percent_stacked_bar_chart');
     const data = await dataByChartId('percent_stacked_bar_chart');
     const myAdvisor = new Advisor();
     const advices = myAdvisor.advise({ data });
@@ -18,7 +16,6 @@ describe('should advise percent_stacked_bar', () => {
 describe('should NOT advise bar/bar', () => {
   test('categrorical field should not be x-axis of bar chart', async () => {
     const data = await dataByChartId('line_chart');
-
     const myAdvisor = new Advisor();
     const advices = myAdvisor.advise({ data });
     expect(advices[0].type === 'percent_stacked_bar_chart').toBe(false);
