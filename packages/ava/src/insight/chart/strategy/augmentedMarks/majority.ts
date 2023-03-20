4f77f0d2fa0474628c70f22d0547777c4b4ad4e4
pddpd <pddpengdi@gmail.com>
2023-03-20 16:39:29 +0800

    docs(site): add insight visualization demo (#594)
    * docs(site): adjust insight demos
    
    * docs(site): add insight visualization demo
    
    * fix(ava/insight): handle ts error

--- packages/ava/src/insight/chart/strategy/augmentedMarks/majority.ts
+++ packages/ava/src/insight/chart/strategy/augmentedMarks/majority.ts
@@ -4,7 +4,7 @@ import { MajorityInfo, InsightInfo } from '../../../types';
 import { insight2ChartStrategy } from '../chart';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const majorityStrategy = (insight: InsightInfo<MajorityInfo>, patterns: MajorityInfo[]): Mark[] => {
+export const majorityStrategy = (insight: InsightInfo<MajorityInfo>, _patterns: MajorityInfo[]): Mark[] => {
   const chartMark = insight2ChartStrategy(insight);
   return [chartMark];
 };
