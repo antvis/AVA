e57b86efff4987a7af9408e463bf5fa16acaa98d
chenluli <chenluuli@gmail.com>
2023-03-21 15:11:04 +0800

    feat(ava/insight): format y axis label by default

--- packages/ava/src/insight/chart/strategy/viewSpec.ts
+++ packages/ava/src/insight/chart/strategy/viewSpec.ts
@@ -1,12 +1,14 @@
 import { G2Spec, Mark } from '@antv/g2';
 
+import { dataFormat } from '../../../utils';
+
 export const viewSpecStrategy = (marks: Mark[]): G2Spec => {
   return {
     type: 'view',
     theme: 'classic',
     axis: {
       x: { labelAutoHide: true, labelAutoRotate: false, title: false },
-      y: { title: false },
+      y: { title: false, labelFormatter: (d) => dataFormat(d) },
     },
     scale: {
       y: { nice: true },
