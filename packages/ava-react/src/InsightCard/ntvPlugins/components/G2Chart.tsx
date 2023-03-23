a19099469f4a2d7b64d5351975363587e8b04977
chenluli <chenluuli@gmail.com>
2023-03-23 19:06:06 +0800

    fix(ava-react/insight-card): change g2 canvas size according to container size

--- packages/ava-react/src/InsightCard/ntvPlugins/components/G2Chart.tsx
+++ packages/ava-react/src/InsightCard/ntvPlugins/components/G2Chart.tsx
@@ -7,7 +7,6 @@ import { INSIGHT_CARD_PREFIX_CLS } from '../../constants';
 export const G2Chart = ({ spec, height, width }: { spec: G2Spec; height?: number; width?: number }) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const chartRef = React.useRef<Chart>(null);
-
   useEffect(() => {
     if (!chartRef?.current) {
       chartRef.current = new Chart({ container: containerRef?.current, autoFit: true });
@@ -25,6 +24,20 @@ export const G2Chart = ({ spec, height, width }: { spec: G2Spec; height?: number
     }
     chartRef.current.render();
   }, [spec]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      chartRef.current?.options({
+        width: containerRef.current?.clientWidth,
+        height: containerRef.current?.clientHeight,
+      });
+      chartRef.current?.render();
+    };
+    const resizeObserver = new ResizeObserver(handleResize);
+    resizeObserver.observe(containerRef.current);
+
+    return () => resizeObserver.disconnect();
+  }, []);
 
   return (
     <div className={`${INSIGHT_CARD_PREFIX_CLS}-chart-container`}>
