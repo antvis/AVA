973fd8b4f0e1418d2448cff0d2ccea9ec88ad071
chenluli <chenluuli@gmail.com>
2023-03-24 15:41:14 +0800

    fix(ava/insight): change the judgement of enable y nice to viewSpecStrategy

--- packages/ava/src/insight/chart/generator/insights.ts
+++ packages/ava/src/insight/chart/generator/insights.ts
@@ -51,13 +51,6 @@ export function generateInsightChartSpec(insight: InsightInfo<PatternInfo>): G2S
     correlation: correlationStrategy,
   };
 
-  // majority insight pattern visualizes as 'pie', should not use y nice (G2 handle it as rescale y, the pie chart will be less than 100%)
-  const isThetaCoordinate = insight.patterns.map((pattern) => pattern.type).includes('majority');
-  const viewConfig = isThetaCoordinate
-    ? { scale: { y: { nice: false } } }
-    : {
-        scale: { y: { nice: true } },
-      };
   const marks = insightType2Strategy[insightType]?.(insight);
-  return viewSpecStrategy(marks, viewConfig);
+  return viewSpecStrategy(marks, insight);
 }
