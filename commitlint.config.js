18b7cd0d3d48664f58b5880670117dad56108b20
pddpd <pddpengdi@gmail.com>
2023-04-28 12:11:25 +0800

    chore(global): update commit lint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,23 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['chore', 'ci', 'docs', 'feat', 'fix', 'refactor', 'revert', 'style', 'test']],
+    'scope-empty': [2, 'never'],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'global',
+        'ava',
+        'ava/advisor',
+        'ava/ckb',
+        'ava/data',
+        'ava/insight',
+        'ava/ntv',
+        'ava-react',
+        'ava-react/ntv',
+        'ava-react/insight-card',
+      ],
+    ],
+  },
+};
