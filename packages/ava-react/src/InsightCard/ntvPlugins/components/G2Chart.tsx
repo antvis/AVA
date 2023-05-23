beace0e68fdeed8365d2bc7331381c9b164cc815
chenluli <chenluuli@gmail.com>
2023-05-23 10:22:27 +0800

    fix(ava-react/insight-card): fix g2 chart render

--- packages/ava-react/src/InsightCard/ntvPlugins/components/G2Chart.tsx
+++ packages/ava-react/src/InsightCard/ntvPlugins/components/G2Chart.tsx
@@ -7,37 +7,30 @@ import { INSIGHT_CARD_PREFIX_CLS } from '../../constants';
 export const G2Chart = ({ spec, height, width }: { spec: G2Spec; height?: number; width?: number }) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const chartRef = React.useRef<Chart>(null);
-  useEffect(() => {
+  const isRendering = React.useRef(false);
+
+  const renderChart = async () => {
     if (!chartRef?.current) {
-      chartRef.current = new Chart({ container: containerRef?.current, autoFit: true });
-      chartRef.current.options({
-        width: containerRef.current?.clientWidth,
-        height: containerRef.current?.clientHeight,
-        ...spec,
+      chartRef.current = new Chart({
+        container: containerRef?.current,
+        autoFit: true,
+        paddingLeft: 'auto',
       });
+      chartRef.current.options(spec);
     } else {
-      chartRef.current.options({
-        width: containerRef.current?.clientWidth,
-        height: containerRef.current?.clientHeight,
-        ...spec,
-      });
+      chartRef.current.options(spec);
     }
-    chartRef.current.render();
-  }, [spec]);
+
+    if (!isRendering.current) {
+      isRendering.current = true;
+      await chartRef.current?.render();
+      isRendering.current = false;
+    }
+  };
 
   useEffect(() => {
-    const handleResize = () => {
-      chartRef.current?.options({
-        width: containerRef.current?.clientWidth,
-        height: containerRef.current?.clientHeight,
-      });
-      chartRef.current?.render();
-    };
-    const resizeObserver = new ResizeObserver(handleResize);
-    resizeObserver.observe(containerRef.current);
-
-    return () => resizeObserver.disconnect();
-  }, []);
+    renderChart();
+  }, [spec]);
 
   return (
     <div className={`${INSIGHT_CARD_PREFIX_CLS}-chart-container`}>
