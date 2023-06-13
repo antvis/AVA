64dbb2f6e74d0124404a4fed9d9de84f26605ac3
laixingui.lxg <laixingui1608@163.com>
2023-06-13 11:51:49 +0800

    fix(ava/insight): calculate real thresholds in outlier extractor

--- packages/ava/src/insight/insights/util.ts
+++ packages/ava/src/insight/insights/util.ts
@@ -1,16 +1,32 @@
 import { mean } from 'lodash';
 
-import { standardDeviation, cdf } from '../../data';
+import { standardDeviation, cdf, normalDistributionQuantile, max, min } from '../../data';
 
 export const calculatePValue = (
   values: number[],
   target: number,
-  alternative: 'two-sided' | 'less' | 'greater' = 'greater'
+  alternative: 'two-sided' | 'less' | 'greater' = 'two-sided'
 ) => {
   const meanValue = mean(values);
   const std = standardDeviation(values);
   const cdfValue = cdf(target, meanValue, std);
-  if (alternative === 'two-sided') return cdfValue < 0.5 ? cdfValue : 1 - cdfValue;
+  if (alternative === 'two-sided') return cdfValue < 0.5 ? 2 * cdfValue : 2 * (1 - cdfValue);
   if (alternative === 'less') return cdfValue;
   return 1 - cdfValue;
+};
+
+export const calculateOutlierThresholds = (
+  values: number[],
+  significance: number,
+  alternative: 'two-sided' | 'less' | 'greater' = 'two-sided'
+): [number, number] => {
+  const meanValue = mean(values);
+  const std = standardDeviation(values);
+  const p = 1 - significance;
+  if (alternative === 'greater') return [normalDistributionQuantile(significance, meanValue, std), max(values)];
+  if (alternative === 'less') return [min(values), normalDistributionQuantile(p, meanValue, std)];
+  return [
+    normalDistributionQuantile(p / 2, meanValue, std),
+    normalDistributionQuantile(significance + p / 2, meanValue, std),
+  ];
 };
