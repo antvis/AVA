b9e9bebb177522355e4c5d17917269f03f55a9f2
yuran.lcl <lcl266859@antgroup.com>
2023-10-30 14:40:14 +0800

    feat(ava/insight): export insightPatternsExtractor method to extract specific insight patterns

--- packages/ava/src/insight/index.ts
+++ packages/ava/src/insight/index.ts
@@ -1,3 +1,4 @@
 export { getInsights } from './pipeline';
+export { insightPatternsExtractor } from './insights';
 export { generateInsightVisualizationSpec } from './pipeline/visualize';
 export * from './types';
