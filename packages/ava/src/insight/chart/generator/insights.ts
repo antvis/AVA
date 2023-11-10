a41aba0dcf744f2cbe72bfbdb19911871ac91716
laixingui.lxg <laixingui1608@163.com>
2023-11-10 11:54:09 +0800

    fix(ava/insight): delete generateInsightAugmentedMarks method

--- packages/ava/src/insight/chart/generator/insights.ts
+++ packages/ava/src/insight/chart/generator/insights.ts
@@ -9,34 +9,8 @@ import {
   correlationStrategy,
   timeSeriesOutlierStrategy,
   trendStrategy,
-  trendAugmentedMarksStrategy,
-  categoryOutlierAugmentedMarksStrategy,
-  changePointAugmentedMarksStrategy,
-  lowVarianceAugmentedMarkStrategy,
   viewSpecStrategy,
 } from '../strategy';
-import { LineMarkConfig, PointMarkConfig, TextMarkConfig } from '../types';
-
-// The generateInsightAugmentedMarks needs to be refactored, it's not exported now.
-export function generateInsightAugmentedMarks(
-  insight: InsightInfo<PatternInfo>,
-  markStyleConfig?: TextMarkConfig | LineMarkConfig | PointMarkConfig
-): G2Spec {
-  const { type: insightType } = insight?.patterns[0];
-
-  const insightType2Strategy: Record<
-    string,
-    (insight: InsightInfo<PatternInfo>, markStyleConfig?: TextMarkConfig | LineMarkConfig | PointMarkConfig) => Mark[]
-  > = {
-    trend: trendAugmentedMarksStrategy,
-    category_outlier: categoryOutlierAugmentedMarksStrategy,
-    change_point: changePointAugmentedMarksStrategy,
-    low_variance: lowVarianceAugmentedMarkStrategy,
-  };
-
-  const augmentedMarks = insightType2Strategy[insightType]?.(insight, markStyleConfig);
-  return augmentedMarks;
-}
 
 export function generateInsightChartSpec(insight: InsightInfo<PatternInfo>): G2Spec {
   const { type: insightType } = insight.patterns[0];
