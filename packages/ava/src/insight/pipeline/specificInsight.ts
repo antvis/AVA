a67e69b70abbe494c6e3b9749e2c337c1b03dac1
laixingui.lxg <laixingui1608@163.com>
2023-11-13 18:02:44 +0800

    fix(ava/insight): only generate annotationSpec for valid patternInfo

--- packages/ava/src/insight/pipeline/specificInsight.ts
+++ packages/ava/src/insight/pipeline/specificInsight.ts
@@ -1,23 +1,28 @@
+import { size } from 'lodash';
+
 import {
   generateInsightChartSpec,
   changePointAugmentedMarksStrategy,
   trendAugmentedMarksStrategy,
   timeSeriesOutlierStrategyAugmentedMarksStrategy,
   AugmentedMarks,
+  lowVarianceAugmentedMarkStrategy,
+  categoryOutlierAugmentedMarksStrategy,
 } from '../chart';
 import { insightPatternsExtractor } from '../insights';
 import {
   InsightInfo,
+  InsightType,
   PatternInfo,
   PatternInfo2InsightInfoProps,
   SpecificInsightProps,
   SpecificInsightResult,
+  TimeSeriesOutlierInfo,
 } from '../types';
 import generateInsightNarrative from '../narrative';
+import { pickValidPattern, pickValidTimeSeriesOutlierPatterns } from '../insights/util';
 
-export const patternInfo2InsightInfo = (
-  props: PatternInfo2InsightInfoProps
-): Omit<InsightInfo<PatternInfo>, 'score'> => {
+export const patternInfo2InsightInfo = (props: PatternInfo2InsightInfoProps): SpecificInsightResult => {
   const { dimensions, measures, data, patternInfos } = props;
   return {
     subspace: [],
@@ -29,33 +34,54 @@ export const patternInfo2InsightInfo = (
 };
 
 export const getAnnotationSpec = (insightInfo: InsightInfo<PatternInfo>): AugmentedMarks => {
-  const { type: insightType } = insightInfo.patterns[0];
+  const { type: insightType } = insightInfo.patterns[0] ?? {};
 
   const insightType2AugmentedMarks = {
     trend: trendAugmentedMarksStrategy,
     change_point: changePointAugmentedMarksStrategy,
     time_series_outlier: timeSeriesOutlierStrategyAugmentedMarksStrategy,
+    low_variance: lowVarianceAugmentedMarkStrategy,
+    category_outlier: categoryOutlierAugmentedMarksStrategy,
   };
   return insightType2AugmentedMarks[insightType]?.(insightInfo);
 };
 
-export const getSpecificInsight = (props: SpecificInsightProps): SpecificInsightResult => {
-  const { visualizationOptions = { lang: 'zh-CN' } } = props.options || {};
-  const patternInfos = insightPatternsExtractor(props);
-  const insightInfo = patternInfo2InsightInfo({ ...props, patternInfos });
-  const annotationSpec = getAnnotationSpec(insightInfo);
-  const chartSpec = generateInsightChartSpec(insightInfo);
-  const narrativeSpec = generateInsightNarrative(insightInfo, visualizationOptions);
+export const filterValidInsightInfoForAnnotationSpec = ({
+  patternInfos = [],
+  insightType,
+}: {
+  patternInfos: PatternInfo[];
+  insightType: InsightType;
+}) => {
+  if (insightType === 'time_series_outlier')
+    return pickValidTimeSeriesOutlierPatterns(patternInfos as TimeSeriesOutlierInfo[]);
+  return pickValidPattern(patternInfos);
+};
 
-  return {
-    ...insightInfo,
-    visualizationSpecs: [
-      {
-        annotationSpec,
-        chartSpec,
-        patternType: props.insightType,
-        narrativeSpec,
-      },
-    ],
-  };
+export const getSpecificInsight = (props: SpecificInsightProps): SpecificInsightResult => {
+  const { options = {}, insightType } = props;
+  const { visualizationOptions = { lang: 'zh-CN' } } = options;
+  const patternInfos = insightPatternsExtractor(props);
+  const validPatternInfos = filterValidInsightInfoForAnnotationSpec({ patternInfos, insightType });
+  const totalInsightInfo = patternInfo2InsightInfo({ ...props, patternInfos });
+
+  if (size(validPatternInfos)) {
+    const validInsightInfo = patternInfo2InsightInfo({ ...props, patternInfos: validPatternInfos });
+    const annotationSpec = getAnnotationSpec(validInsightInfo);
+    const chartSpec = generateInsightChartSpec(validInsightInfo);
+    const narrativeSpec = generateInsightNarrative(validInsightInfo, visualizationOptions);
+    return {
+      ...totalInsightInfo,
+      visualizationSpecs: [
+        {
+          annotationSpec,
+          chartSpec,
+          patternType: props.insightType,
+          narrativeSpec,
+        },
+      ],
+    };
+  }
+
+  return { ...totalInsightInfo };
 };
