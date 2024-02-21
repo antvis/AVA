704343080e352b57059764d3b560e076072224c0
chenluli <chenluuli@gmail.com>
2024-02-21 11:36:52 +0800

    feat(ava/data): add function to parse ISO datetime string to js Date type

--- packages/ava/src/data/utils/isType/constants.ts
+++ packages/ava/src/data/utils/isType/constants.ts
@@ -10,14 +10,16 @@ export const SPECIAL_BOOLEANS = [
 
 // For isDateString.ts
 export const DELIMITER = '([-_./\\s])';
-export const YEAR = '(18|19|20)\\d{2}';
-export const MONTH = '(0?[1-9]|1[012])';
-export const DAY = '(0?[1-9]|[12]\\d|3[01])';
-export const WEEK = '([0-4]\\d|5[0-2])';
-export const WEEKDAY = '([1-7])';
-export const HOUR = '(0?\\d|1\\d|2[0-4])';
-export const MINUTE = '(0?\\d|[012345]\\d)';
-export const SECOND = MINUTE;
-export const MILLISECOND = '\\d{1,4}';
-export const YEARDAY = '((([0-2]\\d|3[0-5])\\d)|36[0-6])';
-export const OFFSET = `(Z|[+-]${HOUR}(:${MINUTE})?)`;
+export const YEAR = '(?<year>(18|19|20)\\d{2})';
+export const MONTH = '(?<month>0?[1-9]|1[012])';
+export const DAY = '(?<day>0?[1-9]|[12]\\d|3[01])';
+export const WEEK = '(?<week>[0-4]\\d|5[0-2])';
+export const WEEKDAY = '(?<weekday>[1-7])';
+export const BASE_HOUR = '(0?\\d|1\\d|2[0-4])';
+export const BASE_MINUTE = '(0?\\d|[012345]\\d)';
+export const HOUR = `(?<hour>${BASE_MINUTE})`;
+export const MINUTE = `(?<minute>${BASE_MINUTE})`;
+export const SECOND = `(?<second>${BASE_MINUTE})`;
+export const MILLISECOND = '(?<millisecond>\\d{1,4})';
+export const YEARDAY = '(?<yearDay>(([0-2]\\d|3[0-5])\\d)|36[0-6])';
+export const OFFSET = `(?<offset>Z|[+-]${BASE_HOUR}(:${BASE_MINUTE})?)`;
--- packages/ava/src/data/utils/isType/isDateString.ts
+++ packages/ava/src/data/utils/isType/isDateString.ts
@@ -81,3 +81,45 @@ export function isDateString(value: unknown, strictDatePattern?: boolean): value
   }
   return false;
 }
+
+/** parse ISO 8601 date string to standard Date type, if month and date is missing, will use new Date(01-01)
+ * Reference: https://www.cl.cam.ac.uk/~mgk25/iso-time.html
+ * 将日期字符串转为标准 Date 类型，如果没有月日，只有时间信息，会默认为 01-01
+ */
+export function parseIsoDateString(value: string, strictDatePattern: boolean = false): Date | null {
+  const isoDateAndTimeRegs = getIsoDateAndTimeRegs(strictDatePattern);
+  for (let i = 0; i < isoDateAndTimeRegs.length; i += 1) {
+    const reg = isoDateAndTimeRegs[i];
+    if (reg.test(value.trim())) {
+      const matches = value.trim().match(reg);
+      if (matches.groups) {
+        const { year, month, day, week, weekday, hour, minute, second, millisecond, yearDay, offset } =
+          matches.groups || {};
+        const yearNum = parseInt(year, 10);
+        if (yearDay) {
+          return new Date(yearNum, 0, parseInt(yearDay, 10));
+        }
+
+        if (week) {
+          const weekNum = parseInt(week, 10);
+          const weekDayNum = weekday ? parseInt(weekday, 10) : 1;
+          const firstDayOfYear = new Date(yearNum, 0, 1);
+          // 给定年份的第一天是周几
+          const firstDayOfYearDayOfWeek = firstDayOfYear.getDay() === 0 ? 7 : firstDayOfYear.getDay();
+          // 计算第一周的第一天相对 firstDayOfYear 的偏移量
+          const firstWeekStartDayOffset = firstDayOfYearDayOfWeek === 1 ? 1 : 1 - firstDayOfYearDayOfWeek;
+          // 目标日期偏移量
+          const targetDateOffset = (weekNum - 1) * 7 + (weekDayNum + firstWeekStartDayOffset);
+          return new Date(yearNum, 0, targetDateOffset);
+        }
+
+        const formattedDateString = [year, month ?? '01', day ?? '01'].join('-');
+        const formattedTimeString = `${[hour ?? '00', minute ?? '00', second ?? '00'].join(':')}.${
+          millisecond ?? '000'
+        }${offset ?? ''}`;
+        return new Date(`${formattedDateString} ${formattedTimeString}`);
+      }
+    }
+  }
+  return null;
+}
