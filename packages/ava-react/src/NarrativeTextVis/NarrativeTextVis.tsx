c2bc501bfc1a57af2ee0bf9e4666800eb4df8098
bbsqq <1491812683@qq.com>
2024-05-07 19:30:25 +0800

    fix(ava-react/ntv): allow copy ntv by default behavior

--- packages/ava-react/src/NarrativeTextVis/NarrativeTextVis.tsx
+++ packages/ava-react/src/NarrativeTextVis/NarrativeTextVis.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 
 import { v4 } from 'uuid';
+import { isFunction, isNull, isUndefined } from 'lodash';
 
 import { classnames as cx } from '../utils';
 
@@ -52,21 +53,22 @@ export function NarrativeTextVis({
   useEffect(() => {
     const onCopy = async (event: ClipboardEvent) => {
       const { plainText, html } = await getSelectionContentForCopy();
-      // 如果没有传递复制方法，默认行为是拦截用户复制操作(使用快捷键或右键选择复制均会触发)，将转换后的内容放进剪切板
-      // if no `copyNarrative` passed in, the default behavior when user conduct `copy` is to put the transformed html and plainText into user's clipboard
-      if (!copyNarrative) {
+      if (isFunction(copyNarrative)) {
+        copyNarrative({ spec, plainText, html });
+      } else if (isUndefined(copyNarrative)) {
         // 仅成功解析出 plainText 才拦截处理，其他情况下走默认处理
+        // 如果没有传递复制方法，默认行为是拦截用户复制操作(使用快捷键或右键选择复制均会触发)，将转换后的内容放进剪切板
+        // if no `copyNarrative` passed in, the default behavior when user conduct `copy` is to put the transformed html and plainText into user's clipboard
         // TODO @羽然 此处修改逻辑仅针对复制 disabled Input 的情况，还有更多情况待进一步兼容
         if (plainText) {
           event.preventDefault();
           copyToClipboard(html, plainText, onCopySuccess, onCopyFailure);
         }
-      } else {
-        copyNarrative({ spec, plainText, html });
       }
     };
-
-    narrativeDomRef.current?.addEventListener('copy', onCopy);
+    if (!isNull(copyNarrative)) {
+      narrativeDomRef.current?.addEventListener('copy', onCopy);
+    }
     return () => {
       narrativeDomRef.current?.removeEventListener('copy', onCopy);
     };
