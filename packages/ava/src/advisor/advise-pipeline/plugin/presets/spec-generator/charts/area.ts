68057f3e7ba77a3d2d62fb90d28099b5c157b688
chenluli <chenluuli@gmail.com>
2024-06-07 18:32:01 +0800

    fix(ava/advisor): fix single datapoint line chart and area chart

--- packages/ava/src/advisor/advise-pipeline/plugin/presets/spec-generator/charts/area.ts
+++ packages/ava/src/advisor/advise-pipeline/plugin/presets/spec-generator/charts/area.ts
@@ -1,7 +1,8 @@
 import { hasSubset, intersects } from '../../../../../utils';
 import { splitAreaXYSeries } from '../../visual-encoder/split-fields';
+import { getLineSize } from '../../visual-encoder/utils';
 
-import type { Data } from '../../../../../../common/types';
+import type { Data, Datum } from '../../../../../../common/types';
 import type { Advice, BasicDataPropertyForAdvice } from '../../../../../types';
 
 export function areaChart(data: Data, dataProps: BasicDataPropertyForAdvice[]): Advice['spec'] {
@@ -16,6 +17,10 @@ export function areaChart(data: Data, dataProps: BasicDataPropertyForAdvice[]): 
     encode: {
       x: field4X.name,
       y: field4Y.name,
+      size: (datum: Datum) => getLineSize(datum, data, { field4X }),
+    },
+    legend: {
+      size: false,
     },
   };
 
@@ -33,6 +38,10 @@ export function stackedAreaChart(data: Data, dataProps: BasicDataPropertyForAdvi
       x: field4X.name,
       y: field4Y.name,
       color: field4Series.name,
+      size: (datum: Datum) => getLineSize(datum, data, { field4Split: field4Series, field4X }),
+    },
+    legend: {
+      size: false,
     },
     transform: [{ type: 'stackY' }],
   };
