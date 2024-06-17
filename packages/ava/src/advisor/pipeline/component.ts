1badd46abecc7dc90808f6295dca9f9d844b5a27
chenluli <chenluuli@gmail.com>
2024-06-17 16:59:39 +0800

    feat(ava/advisor): add async and condition type to advisor plugin

--- packages/ava/src/advisor/pipeline/component.ts
+++ packages/ava/src/advisor/pipeline/component.ts
@@ -1,5 +1,4 @@
-import { AsyncParallelHook, SyncHook } from 'tapable';
-import { last } from 'lodash';
+import { isFunction, last } from 'lodash';
 
 import type { AdvisorPluginType, AdvisorPipelineContext } from '../types';
 
@@ -12,11 +11,7 @@ export class BaseComponent<Input = any, Output = any> {
   plugins: AdvisorPluginType<Input, Output>[] = [];
 
   /** 是否存在异步插件 */
-  private hasAsyncPlugin: boolean;
-
-  pluginManager: AsyncParallelHook<[Input, PluginResultMap<Output>], Output>;
-
-  syncPluginManager: SyncHook<[Input, PluginResultMap<Output>], Output>;
+  private hasAsyncPlugin: boolean = false;
 
   afterPluginsExecute?: (params: PluginResultMap<Output>, context?: AdvisorPipelineContext) => Output;
 
@@ -32,8 +27,6 @@ export class BaseComponent<Input = any, Output = any> {
   ) {
     this.name = name;
     this.afterPluginsExecute = options?.afterPluginsExecute ?? this.defaultAfterPluginsExecute;
-    this.pluginManager = new AsyncParallelHook(['data', 'results']);
-    this.syncPluginManager = new SyncHook(['data', 'results']);
     this.context = options?.context;
     this.hasAsyncPlugin = !!options?.plugins?.find((plugin) => this.isPluginAsync(plugin));
     options?.plugins?.forEach((plugin) => {
@@ -41,44 +34,29 @@ export class BaseComponent<Input = any, Output = any> {
     });
   }
 
-  private defaultAfterPluginsExecute(params: Record<string, Output>) {
-    return last(Object.values(params));
+  private defaultAfterPluginsExecute(params: Record<string, Output | null>) {
+    if (this.plugins.length) {
+      const lastPlugin = last(this.plugins);
+      return params[lastPlugin.name];
+    }
+    return null;
   }
 
   private isPluginAsync(plugin: AdvisorPluginType) {
-    // 检测插件是否为异步的，并设置hasAsyncPlugin标志位
-    if (plugin.execute.constructor.name === 'AsyncFunction') {
+    // 检测插件是否为异步的
+    if (plugin.type === 'async' || plugin.execute.constructor.name === 'AsyncFunction') {
       return true;
     }
     return false;
   }
 
-  // 处理 之前都是同步的插件，新追加注册一个异步的插件 的情况 -- 需要执行的地方就不能用
   registerPlugin(plugin: AdvisorPluginType) {
     plugin.onLoad?.(this.context);
-    this.plugins.push(plugin);
     if (this.isPluginAsync(plugin)) {
       this.hasAsyncPlugin = true;
     }
 
-    if (this.hasAsyncPlugin) {
-      this.pluginManager.tapPromise(plugin.name, async (input, outputs = {}) => {
-        plugin.onBeforeExecute?.(input, this.context);
-        const output = await plugin.execute(input, this.context);
-        plugin.onAfterExecute?.(output, this.context);
-        /* eslint-disable no-param-reassign */
-        outputs[plugin.name] = output;
-      });
-    } else {
-      this.syncPluginManager.tap(plugin.name, (input, outputs = {}) => {
-        plugin.onBeforeExecute?.(input, this.context);
-        const output = plugin.execute(input, this.context);
-        plugin.onAfterExecute?.(output, this.context);
-        /* eslint-disable no-param-reassign */
-        outputs[plugin.name] = output;
-        return output;
-      });
-    }
+    this.plugins.push(plugin);
   }
 
   unloadPlugin(pluginName: string) {
@@ -89,16 +67,37 @@ export class BaseComponent<Input = any, Output = any> {
     }
   }
 
-  execute(params: Input): Output | Promise<Output> {
+  execute(params: Input): Output {
     if (this.hasAsyncPlugin) {
-      // console.warn('存在异步执行的插件，建议使用 executeAsync')
-      const pluginsOutput = {};
-      return this.pluginManager.promise(params, pluginsOutput).then(async () => {
-        return this.afterPluginsExecute?.(pluginsOutput, this.context);
-      });
+      // eslint-disable-next-line no-console
+      console.warn('存在异步执行的插件，请使用 executeAsync');
     }
     const pluginsOutput = {};
-    this.syncPluginManager.call(params, pluginsOutput);
+    this.plugins.forEach((plugin) => {
+      plugin.onBeforeExecute?.(params, this.context);
+      if (isFunction(plugin.condition) && !plugin.condition(params, this.context)) return;
+      const output = plugin.execute(params, this.context) as Output;
+      plugin.onAfterExecute?.(output, this.context);
+      pluginsOutput[plugin.name] = output;
+    });
     return this.afterPluginsExecute?.(pluginsOutput, this.context);
+  }
+
+  async executeAsync(params: Input): Promise<Output> {
+    if (!this.hasAsyncPlugin) {
+      return Promise.resolve(this.execute(params));
+    }
+    const pluginsOutput = {};
+    return Promise.all(
+      this.plugins.map(async (plugin) => {
+        plugin.onBeforeExecute?.(params, this.context);
+        if (isFunction(plugin.condition) && !plugin.condition(params, this.context)) return;
+        const output = (await plugin.execute(params, this.context)) as Output;
+        plugin.onAfterExecute?.(output, this.context);
+        pluginsOutput[plugin.name] = output;
+      })
+    ).then(async () => {
+      return this.afterPluginsExecute?.(pluginsOutput, this.context);
+    });
   }
 }
