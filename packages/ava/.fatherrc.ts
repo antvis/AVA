db77d5bbce8161513663e3be88b40a1dd8f45615
Yanz <yanzhi.private@gmail.com>
2024-12-10 21:18:03 +0800

    ci(ava/advisor): update commonjs and esmodule build config (#796)
    * ci(ava/advisor): update commonjs and esmodule build config
    
    * ci(ava/advisor): update tsconfig name
    
    ---------
    
    Co-authored-by: yanzhi.yan <yanzhi.yan@antgroup.com>

--- packages/ava/.fatherrc.ts
+++ packages/ava/.fatherrc.ts
@@ -1,3 +1,9 @@
 import fatherConfig from '../../.fatherrc';
+import path from 'path';
 
-export default fatherConfig('ts', 'AVA');
+export default fatherConfig('ts', 'AVA', {
+  alias: {
+    '@ava': path.resolve(__dirname, 'src'),
+    '@advisor': path.resolve(__dirname, 'src/advisor'),
+  }
+});
