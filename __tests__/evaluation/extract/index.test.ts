7a5ebadda5f0a2b726169605499f73e28bbb86f2
hustcc <i@hust.cc>
2025-11-06 19:43:45 +0800

    test: fix test case

---
+++ __tests__/evaluation/extract/index.test.ts
@@ -0,0 +1,5 @@
+describe('extract evaluation', () => {
+  it('should evaluate the extracted data', async () => {
+    expect(1).toBe(1);
+  });
+});
--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,5 @@
 
 require('dotenv').config({
-  path: ['.env.local']
+  path: ['.env']
 });
 jest.setTimeout(30000);
