808e4df50808e622ba0ae878089067f7ea2c86ee
xueren.dt <xueren.dt@antgroup.com>
2025-11-18 16:37:13 +0800

    fix(ava): fix auto cr problem

--- __tests__/evaluation/evaluate-advise/unspecified-chart.test.ts
+++ __tests__/evaluation/evaluate-advise/unspecified-chart.test.ts
@@ -4,25 +4,26 @@ import { validateObject } from '../../../src/utils/validator';
 import { CHARTS } from '../../../src/ckb';
 import { Spec } from '../../../src/types';
 
+const equivalentSets = [
+  new Set(['line', 'area', 'column', 'bar']),
+  new Set(['bar', 'column', 'pie']),
+  new Set(['violin', 'boxplot']),
+  new Set(['word-cloud', 'bar', 'column']),
+  new Set(['indented-tree', 'organization-chart']),
+  new Set(['radar', 'bar', 'column']),
+];
+
+const typeMatches = (a?: string, b?: string) => {
+  if (!a || !b) return false;
+  if (a === b) return true;
+  return equivalentSets.some((set) => set.has(a) && set.has(b));
+};
+
 // 未指定图表类型的评测，评测 advise 方法
 runAdviseEvaluation(
   (d: TestData) => d.questionWithoutChart,
   (spec: Spec, answer: TestData['answer']) => {
     const { type, ...finalSpec } = spec || {};
-    // 未指定图表类型时，以下图表类型认为是等价的
-    const equivalentSets = [
-      new Set(['line', 'area', 'column', 'bar']),
-      new Set(['bar', 'column', 'pie']),
-      new Set(['violin', 'boxplot']),
-      new Set(['word-cloud', 'bar', 'column']),
-      new Set(['indented-tree', 'organization-chart']),
-      new Set(['radar', 'bar', 'column']),
-    ];
-    const typeMatches = (a?: string, b?: string) => {
-      if (!a || !b) return false;
-      if (a === b) return true;
-      return equivalentSets.some((set) => set.has(a) && set.has(b));
-    };
-    return typeMatches(type, answer.type) && CHARTS[type] && validateObject(CHARTS[type].zodSchema, finalSpec) === true;
+    return typeMatches(type, answer.type) && validateObject(CHARTS[type].zodSchema, finalSpec) === true;
   }
 );
