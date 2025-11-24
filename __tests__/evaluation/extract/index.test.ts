c4482c737f0434d7bd86c830d1b7091b15cfbf39
Yanz <yanzhi.private@gmail.com>
2025-11-24 09:40:07 +0800

    chore: add logger write through evaluate advise (#895)
    Co-authored-by: yanzhi.yan <yanzhi.yan@antgroup.com>

--- __tests__/evaluation/extract/index.test.ts
+++ __tests__/evaluation/extract/index.test.ts
@@ -4,7 +4,7 @@ import { generate } from './generate';
 import { evaluate } from './evaluate';
 import { EVALUATE_RESULT_LOG_PATH } from './constants';
 
-describe('expect evaluate pass rate >= 90%', () => {
+describe('expect evaluate pass rate >= 95%', () => {
   it('generate success rate >= 95%', async () => {
     const { success, total } = await generate();
     expect(success / total).toBeGreaterThanOrEqual(0.95);
@@ -15,9 +15,12 @@ describe('expect evaluate pass rate >= 90%', () => {
     expect(success / total).toBeGreaterThanOrEqual(0.95);
   }, 6000000);
 
-  it('evaluate extract pass rate >= 90%', () => {
+  it('evaluate extract pass rate >= 95%', () => {
     const content = fs.readFileSync(EVALUATE_RESULT_LOG_PATH);
-    const lines = content.toString().split('\n').filter(line => line.length > 0);
+    const lines = content
+      .toString()
+      .split('\n')
+      .filter((line) => line.length > 0);
     let total = lines.length;
     let pass = 0;
 
