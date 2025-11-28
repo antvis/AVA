5eefd57e57ccc25dcb833a05d952780037546916
Yanz <yanzhi.private@gmail.com>
2025-11-28 11:29:31 +0800

    Docs/llm proxy (#903)
    * docs: add llm proxy to site demo
    
    * docs: add llm proxy in playground
    
    ---------
    
    Co-authored-by: yanzhi.yan <yanzhi.yan@antgroup.com>

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        tags: [
+          {
+            tag: 'script',
+            attrs: { src: 'https://gw.alipayobjects.com/os/lib/axios/1.12.2/dist/axios.min.js' },
+            injectTo: 'head',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 3001,
     open: true,
@@ -12,5 +26,10 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['@antv/ava'],
     include: ['color-blind', 'quantize'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['axios'],
+    },
   },
 });
