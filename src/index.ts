98e1c5fc87f58715aa57322e17867d4f46b1c7df
chenying <cy01387976@antgroup.com>
2026-05-14 09:57:18 +0800

    feat: 修复类型错误

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@
  * Main entry point
  */
 export { AVA } from './ava';
-export type { AVAConfig, LLMConfig, AnalysisResponse, DatasetInfo, FieldMetadata, ChartType, SuggestResult } from './types';
+export type { AVAConfig, LLMConfig, AnalysisResponse, AnalysisStep, StepStatus, DatasetInfo, FieldMetadata, ChartType, SuggestResult } from './types';
 export { loadCSV, loadObject, loadURL, loadText, extractMetadata, formatDatasetInfo } from './data';
